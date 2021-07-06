b2f8bfa2f01aba96b50df4e36574b800b75a27a7
kwonjaeeun <dino218@naver.com>
2021-07-06 15:13:15 +0900

    Initial commit

---
+++ src/main/java/com/gg/mlg/mlg/MlgApplication.java
@@ -0,0 +1,13 @@
+package com.gg.mlg.mlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gg/mlg/mlg/MlgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.mlg.mlg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
