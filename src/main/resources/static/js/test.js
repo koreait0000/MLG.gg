bb722adb775d2884140fb5036d93fdeecaa03563
hyung000620 <dksms1@naver.com>
2021-08-03 11:59:03 +0900

    header 반응형 작업

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector('.header-menu-toggle');
+
+toggleButton.addEventListener("click", () =>{
+    const menu = document.querySelector('.header-nav');
+    menu.style.display = "block";
+    menu.style.top= "100px";
+})
